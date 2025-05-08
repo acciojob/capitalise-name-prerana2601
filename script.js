cbd29694f11f8226ec1c673e3f575419aad7b276
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2025-05-08 16:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//your JS code here. If required.
-// Function to capitalize the name when the input field loses focus
-function capitalizeName() {
-    // Get the input field by id 'fname'
+function convertToUppercase() {
     var inputField = document.getElementById("fname");
-    // Convert the value to uppercase and update the input field
     inputField.value = inputField.value.toUpperCase();
 }
